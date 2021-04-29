320593976bd5b2d6c9554f4ba2951846c98cd220
Ivorbul <buleeff.igor@yandex.ru>
2021-04-29 21:08:59 +0300

    Добавлен модуль string.py Добавлена функция возведения в степень

--- git_learning/nums.py
+++ git_learning/nums.py
@@ -7,7 +7,11 @@ def minus(a, b):
 def multiplicate(a, b):
     return a*b
 
+def multimulti(a, b):
+    return a**b
+
 if __name__ == "__main__":
     print(plus(2,3))
     print(minus(5,3))
     print(multiplicate(5,3))
+    print(multimulti(5,3))
