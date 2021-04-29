e9bd30a99c790819e179c3468e503ac080ae9c66
Ivorbul <buleeff.igor@yandex.ru>
2021-04-29 20:54:09 +0300

    Добавлена фунция умножить

--- git_learning/nums.py
+++ git_learning/nums.py
@@ -3,6 +3,11 @@ def plus(a, b):
 
 def minus(a, b):
     return a-b
+
+def multiplicate(a, b):
+    return a*b
+
 if __name__ == "__main__":
     print(plus(2,3))
     print(minus(5,3))
+    print(multiplicate(5,3))
